b9f514b2a478853b74fddb8c1cc91cae72519236
Neeraj Madisetty <nmadisetty@nmadisetty-a01.vmware.com>
2022-06-24 17:44:36 -0700

    Routing, Editing and Adding post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,31 @@ import { PostListComponent } from './post-list/post-list.component';
 import { PostComponent } from './post/post.component';
 import { PostEditComponent } from './post-edit/post-edit.component';
 import { AuthComponent } from './auth/auth.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : '/post-list',
+    pathMatch : 'full',
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'post-add',
+    component: PostEditComponent,
+  },
+  {
+    path: 'post-edit/:index',
+    component: PostEditComponent,
+  },
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +47,8 @@ import { AuthComponent } from './auth/auth.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Route, Router } from '@angular/router';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.css']
+  styleUrls: ['./post-edit.component.css'],
 })
 export class PostEditComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  index: number = 0;
+  editMode = false;
+  constructor(private postService: PostService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+
+    let title = '';
+    let description = '';
+    let imagePath = '';
+
+    this.route.params.subscribe((params: Params) => {
+        if(params['index']){
+            console.log(params['index']);
+        }
+
+        this.index = params['index'];
+        const post = this.postService.getPost(this.index);
+        title = post.title;
+        description = post.description;
+        imagePath = post.imagePath;
+
+        this.editMode = true;
+    });
+    this.form = new FormGroup({
+      Title: new FormControl(title, [
+        Validators.required,
+        Validators.maxLength(10),
+      ]),
+      Description: new FormControl(description),
+      ImagePath: new FormControl(imagePath),
+    });
   }
 
+  onSubmit() {
+    console.log('onSubmit called');
+    const title = this.form.value.Title;
+    const description = this.form.value.Description;
+    const imagePath = this.form.value.ImagePath;
+
+    const post: Post = new Post(
+      title,
+      description,
+      imagePath,
+      "test@gmail.com",
+      new Date()
+    );
+    
+    if(this.editMode){
+      this.postService.updatePost(this.index,post);
+    }else{
+      this.postService.addPost(post);
+    }
+    this.router.navigate(['/post-list']);
+  }
 }
